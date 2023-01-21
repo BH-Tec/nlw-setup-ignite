f45d0fc9b946ccff74401c3627178faca3b5453a
Bruno Henrique <brunodorea@outlook.com.br>
2023-01-21 14:27:13 -0300

    ➕ build: ajustes no código (aula 5)

--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -68,7 +68,7 @@ export function NewHabitForm() {
               onCheckedChange={() => handleToggleWeekDay(index)}
             >
               <div
-                className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'
+                className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500 transition-colors group-focus:ring-2 group-focus:ring-violet-600 group-focus:ring-offset-2 group-focus:ring-offset-zinc-900'
               >
                 <Checkbox.CheckboxIndicator>
                   <Check size={20} className="text-white"/>
